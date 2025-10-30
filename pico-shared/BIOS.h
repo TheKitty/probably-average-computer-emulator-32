08ced3341396a9f8b0892067e22ca1172ec6961f
Charlie Birks <charlie@daft.games>
2025-10-30 19:09:45 +0000

    Fix vgabios symbol names
    Yet again I missed a bit

--- pico-shared/BIOS.h
+++ pico-shared/BIOS.h
@@ -3,5 +3,5 @@
 extern char _binary_bios_bin_start[];
 extern char _binary_bios_bin_end[];
 
-extern char _binary_fixed_disk_bios_rom_start[];
-extern char _binary_fixed_disk_bios_rom_end[];
+extern char _binary_vgabios_bin_start[];
+extern char _binary_vgabios_bin_end[];
